7ba000213e6e0c22061d0d9e224015d7feea5738
joshbcondie <joshbcondie@gmail.com>
2015-09-29 14:57:54 -0600

    Copy goodrich0.py into visibility_graph_agent.py

--- bots/visibility_graph_agent.py
+++ bots/visibility_graph_agent.py
@@ -1,27 +1,7 @@
 #!/usr/bin/python -tt
 
-from bzrc import BZRC, Command
-import sys, math, time
-
-# An incredibly simple agent.  All we do is find the closest enemy tank, drive
-# towards it, and shoot.  Note that if friendly fire is allowed, you will very
-# often kill your own tanks with this code.
-
-#################################################################
-# NOTE TO STUDENTS
-# This is a starting point for you.  You will need to greatly
-# modify this code if you want to do anything useful.  But this
-# should help you to know how to interact with BZRC in order to
-# get the information you need.
-# 
-# After starting the bzrflag server, this is one way to start
-# this code:
-# python agent0.py [hostname] [port]
-# 
-# Often this translates to something like the following (with the
-# port name being printed out by the bzrflag server):
-# python agent0.py localhost 49857
-#################################################################
+from bzrc import BZRC, Command, Answer
+import sys, math, time, random
 
 class Agent(object):
 
@@ -29,51 +9,104 @@ class Agent(object):
         self.bzrc = bzrc
         self.constants = self.bzrc.get_constants()
         self.commands = []
+        bases = self.bzrc.get_bases()
+        for base in bases:
+            if base.color == self.constants['team']:
+                self.base = Answer()
+                self.base.x = (base.corner1_x+base.corner3_x)/2
+                self.base.y = (base.corner1_y+base.corner3_y)/2
+        self.update()
+        self.past_position = {}
+        self.goals = {}
+        self.stuck = {}
+        for tank in self.mytanks:
+            self.past_position[tank.index] = tank.x, tank.y
+            self.goals[tank.index] = None
+            self.stuck[tank.index] = 0
+        self.set_flag_goals()
+
+    def update(self):
+        mytanks, othertanks, flags, shots = self.bzrc.get_lots_o_stuff()
+        self.mytanks = mytanks
+        self.othertanks = othertanks
+        self.flags = flags
+        #occg = list(self.bzrc.get_occgrid(i.index) for i in self.mytanks)
+
+    def set_flag_goals(self):
+        for tank in self.mytanks:
+            best = [0,None]
+            flag = self.closest_flag(tank)
+            if flag:
+                self.goals[tank.index] = flag.x,flag.y
 
     def tick(self, time_diff):
         '''Some time has passed; decide what to do next'''
         # Get information from the BZRC server
-        mytanks, othertanks, flags, shots = self.bzrc.get_lots_o_stuff()
-        self.mytanks = mytanks[:1]
-        self.othertanks = othertanks
-        self.flags = flags
-        self.shots = shots
-        self.enemies = [tank for tank in othertanks if tank.color !=
-                self.constants['team']]
+        self.update()
 
         # Reset my set of commands (we don't want to run old commands)
         self.commands = []
 
         # Decide what to do with each of my tanks
         for bot in self.mytanks:
-            self.attack_enemies(bot)
+            self.update_goal(bot)
+            self.past_position[bot.index] = bot.x, bot.y
 
         # Send the commands to the server
         results = self.bzrc.do_commands(self.commands)
 
-    def attack_enemies(self, bot):
-        '''Find the closest enemy and chase it, shooting as you go'''
-        best_enemy = None
-        best_dist = 2 * float(self.constants['worldsize'])
-        for enemy in self.enemies:
-            if enemy.status != 'alive':
-                continue
-            dist = math.sqrt((enemy.x - bot.x)**2 + (enemy.y - bot.y)**2)
-            if dist < best_dist:
-                best_dist = dist
-                best_enemy = enemy
-        if best_enemy is None:
-            command = Command(bot.index, 0, 0, False)
-            self.commands.append(command)
-        else:
-            self.move_to_position(bot, best_enemy.x, best_enemy.y)
+    def update_goal(self, bot):
+        if bot.flag != '-':
+            self.goals[bot.index] = self.base.x, self.base.y
+        if not self.goals[bot.index]:
+            flag = self.closest_flag(bot)
+            if flag:
+                self.goals[bot.index] = flag.x, flag.y
+            else:
+                self.goals[bot.index] = self.random_pos()
+        #elif (bot.x, bot.y) == self.past_position[bot.index]:
+            #self.goals[bot.index] = self.random_pos()
 
+        x,y = self.goals[bot.index]
+        dx = x - bot.x
+        dy = y - bot.y
+        dist = math.sqrt(dx**2 + dy**2)
+        if dist < 10:
+            self.goals[bot.index] = None
+            return
+        self.move_to_position(bot, x, y)
+        #self.commands.append(GoodrichCommand(bot.index, dx/5, dy/5))
+    
     def move_to_position(self, bot, target_x, target_y):
         target_angle = math.atan2(target_y - bot.y,
                 target_x - bot.x)
         relative_angle = self.normalize_angle(target_angle - bot.angle)
         command = Command(bot.index, 1, 2 * relative_angle, True)
         self.commands.append(command)
+
+    def random_pos(self):
+        width = int(self.constants['worldsize'])
+        x = random.randrange(width) - width/2
+        y = random.randrange(width) - width/2
+        return x,y
+
+    def closest_flag(self, bot):
+        best = None
+        for flag in self.flags:
+            if flag.color == self.constants['team']:
+                continue
+            if flag.poss_color != 'none':
+                continue
+            dist = math.sqrt((flag.x - bot.x)**2 + (flag.y - bot.y)**2)
+            if not best or dist < best[0]:
+                best = [dist, flag]
+        if best:
+            return best[1]
+
+    def move_towards(self, bot, x, y):
+        dx = x - bot.x
+        dy = y - bot.y
+        self.commands.append(GoodrichCommand(bot.index, dx/10, dy/10))
 
     def normalize_angle(self, angle):
         '''Make any angle be between +/- pi.'''

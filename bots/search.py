d37d3ca3b1e563c8309b427497ed2d493c13e256
Joshua Mathias <joshuamonkey@gmail.com>
2015-10-07 11:01:43 -0600

    Search

---
+++ bots/search.py
@@ -0,0 +1,58 @@
+# Sample code from http://www.redblobgames.com/pathfinding/
+# Copyright 2014 Red Blob Games <redblobgames@gmail.com>
+#
+# Feel free to use this code in your own projects, including commercial projects
+# License: Apache v2.0 <http://www.apache.org/licenses/LICENSE-2.0.html>
+
+import Queue
+import collections
+
+
+class Queue:
+    def __init__(self):
+        self.elements = collections.deque()
+    
+    def empty(self):
+        return len(self.elements) == 0
+    
+    def put(self, x):
+        self.elements.append(x)
+    
+    def get(self):
+        return self.elements.popleft()
+
+def aStar(graph, weights, start):
+    return came_from, cost_so_far
+
+def bfs(graph, start):
+    goal=len(graph)-1
+    frontier = Queue()
+    frontier.put(start)
+    visited = []
+    visited.append(start)
+    
+    while not frontier.empty():
+        current = frontier.get()
+        if current==goal:
+            return visited
+        # print "current: "+str(current)
+        print("Visiting %r" % current)
+        for node in graph[current]:
+            if node not in visited:
+                frontier.put(node)
+                visited.append(node)
+        print visited
+
+def dfs(graph, start):
+    visited, stack = [], [start]
+    goal=len(graph)-1
+    while stack:
+        vertex = stack.pop()
+        print "vertex: "+str(vertex)
+        print "visited: "+str(visited)
+        if vertex == goal:
+            return visited
+        if vertex not in visited:
+            visited.append(vertex)
+            stack.extend(graph[vertex])
+    return visited

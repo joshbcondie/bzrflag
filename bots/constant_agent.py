02b880046942961e4b97f7f0558a86c7eaf2bd1b
joshbcondie <joshbcondie@gmail.com>
2015-12-02 11:18:13 -0700

    Add agent that always travels in a straight line at a constant velocity

---
+++ bots/constant_agent.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python -tt
+
+from bzrc import BZRC, Command, Answer
+import sys, math, time, random
+
+class Agent(object):
+
+    def __init__(self, bzrc):
+        bzrc.speed(0, 1)
+
+    def tick(self, time_diff):
+        pass
+
+def main():
+    # Process CLI arguments.
+    try:
+        execname, host, port = sys.argv
+    except ValueError:
+        execname = sys.argv[0]
+        print >>sys.stderr, '%s: incorrect number of arguments' % execname
+        print >>sys.stderr, 'usage: %s hostname port' % sys.argv[0]
+        sys.exit(-1)
+
+    # Connect.
+    #bzrc = BZRC(host, int(port), debug=True)
+    bzrc = BZRC(host, int(port))
+
+    agent = Agent(bzrc)
+
+    prev_time = time.time()
+
+    # Run the agent
+    try:
+        while True:
+            time_diff = time.time() - prev_time
+            agent.tick(time_diff)
+    except KeyboardInterrupt:
+        print "Exiting due to keyboard interrupt."
+        bzrc.close()
+
+
+if __name__ == '__main__':
+    main()
+
+# vim: et sw=4 sts=4
